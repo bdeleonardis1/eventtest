65a831e9461b6770a3e9cd0a04dc885fda28c08c
Brian DeLeonardis <bdeleonardis1@gmail.com>
2021-04-13 15:13:02 -0400

    Add e2e test to sample program

---
+++ sampleprogram/e2e_test.go
@@ -0,0 +1,57 @@
+package main_test
+
+import (
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+const (
+	expectedBase = "Enter a number 1-5: "
+)
+
+func TestParity(t *testing.T) {
+	testCases := []struct{
+		input string
+		expected string
+	}{
+		{
+			input: "1",
+			expected: "1 is an odd number",
+		},
+		{
+			input: "2",
+			expected: "2 is an even number",
+		},
+		{
+			input: "11",
+			expected: "11 is an odd number",
+		},
+		{
+			input: "-3",
+			expected: "-3 is an odd number",
+		},
+		{
+			input: "-4",
+			expected: "-4 is an even number",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.input, func(t *testing.T) {
+			cmd := exec.Command("./sampleprogram")
+			cmd.Stdin = strings.NewReader(tc.input)
+			out, err := cmd.Output()
+			if err != nil {
+				t.Fatal(err)
+			}
+			outString := strings.TrimSpace(string(out))
+
+			if outString != expectedBase + tc.expected {
+				t.Errorf("expected '%v', but got '%v'", expectedBase + tc.expected, outString)
+			}
+		})
+	}
+}
+
+
